00d8bbfcd6d9a06548178e6bf838cd2a687a2d13
Grigory Rylov <grishberg@yandex-team.ru>
2020-04-13 11:52:42 +0300

    fixed log level

--- src/main/kotlin/com/github/grishberg/test/launcher/Log4JLogger.kt
+++ src/main/kotlin/com/github/grishberg/test/launcher/Log4JLogger.kt
@@ -1,17 +1,31 @@
 package com.github.grishberg.test.launcher
 
 import com.github.grishberg.tests.common.RunnerLogger
+import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.core.Logger
 
 class Log4JLogger : RunnerLogger {
-    var log = LogManager.getLogger("runner")
+    var log: Logger = LogManager.getLogger("runner") as Logger
+
+    init {
+        log.setLevel(Level.DEBUG);
+    }
 
     override fun i(tag: String, msg: String) {
         log.info("$tag: $msg")
     }
 
     override fun i(tag: String, format: String, vararg args: Any?) {
-        log.info("$tag: $format", args)
+
+        when (args.size) {
+            1 -> log.info("$tag: $format", args[0])
+            2 -> log.info("$tag: $format", args[0], args[1])
+            3 -> log.info("$tag: $format", args[0], args[1], args[2])
+            4 -> log.info("$tag: $format", args[0], args[1], args[2], args[3])
+            5 -> log.info("$tag: $format", args[0], args[1], args[2], args[3], args[4])
+            else -> log.info("$tag: $format", args)
+        }
     }
 
     override fun w(tag: String, msg: String) {
@@ -19,7 +33,14 @@ class Log4JLogger : RunnerLogger {
     }
 
     override fun w(tag: String, format: String, vararg args: Any?) {
-        log.warn("$tag: $format", args)
+        when (args.size) {
+            1 -> log.warn("$tag: $format", args[0])
+            2 -> log.warn("$tag: $format", args[0], args[1])
+            3 -> log.warn("$tag: $format", args[0], args[1], args[2])
+            4 -> log.warn("$tag: $format", args[0], args[1], args[2], args[3])
+            5 -> log.warn("$tag: $format", args[0], args[1], args[2], args[3], args[4])
+            else -> log.warn("$tag: $format", args)
+        }
     }
 
     override fun e(tag: String, msg: String) {
@@ -35,6 +56,13 @@ class Log4JLogger : RunnerLogger {
     }
 
     override fun d(tag: String, format: String, vararg args: Any?) {
-        log.debug("$tag: $format", args)
+        when (args.size) {
+            1 -> log.debug("$tag: $format", args[0])
+            2 -> log.debug("$tag: $format", args[0], args[1])
+            3 -> log.debug("$tag: $format", args[0], args[1], args[2])
+            4 -> log.debug("$tag: $format", args[0], args[1], args[2], args[3])
+            5 -> log.debug("$tag: $format", args[0], args[1], args[2], args[3], args[4])
+            else -> log.debug("$tag: $format", args)
+        }
     }
 }
